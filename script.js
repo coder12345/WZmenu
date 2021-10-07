6cab074eec05c634700b30a18bf717661cfe658b
hellopi2 <jameslego2014@gmail.com>
2021-10-07 19:21:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,44 +25,80 @@ var orderstring5 = [];
 var ordercolo5 = [];
 var orderstring6 = [];
 var ordercolor6 = [];
-
+var addNo = false;
 
 function addToOrder(item, type, side) {
-    
-    if  (currentOrder == 1) {
-        orderstring1.push(item);
-        ordercolor1.push(type);
-        console.log(orderstring1);
+    if (addNo) {
+        if  (currentOrder == 1) {
+            orderstring1.push("No " + item);
+            ordercolor1.push(type);
+            console.log(orderstring1);
+        }
+        if  (currentOrder == 2) {
+            orderstring2.push("No " + item);
+            ordercolor2.push(type);
+            console.log(orderstring2);
+        }
+        if  (currentOrder == 3) {
+            orderstring3.push("No " + item);
+            ordercolor3.push(type);
+            console.log(orderstring3);
+        }
+        if  (currentOrder == 4) {
+            orderstring4.push("No " + item);
+            ordercolor4.push(type);
+            console.log(orderstring4);
+        }
+        if  (currentOrder == 5) {
+            orderstring5.push("No " + item);
+            ordercolor5.push(type);
+            console.log(orderstring5);
+        }
+        if  (currentOrder == 6) {
+            orderstring6.push("No " + item);
+            ordercolor6.push(type);
+            console.log(orderstring6);
+        }
+       addNo = false;
+    } else {
+        if  (currentOrder == 1) {
+            orderstring1.push(item);
+            ordercolor1.push(type);
+            console.log(orderstring1);
+        }
+        if  (currentOrder == 2) {
+            orderstring2.push(item);
+            ordercolor2.push(type);
+            console.log(orderstring2);
+        }
+        if  (currentOrder == 3) {
+            orderstring3.push(item);
+            ordercolor3.push(type);
+            console.log(orderstring3);
+        }
+        if  (currentOrder == 4) {
+            orderstring4.push(item);
+            ordercolor4.push(type);
+            console.log(orderstring4);
+        }
+        if  (currentOrder == 5) {
+            orderstring5.push(item);
+            ordercolor5.push(type);
+            console.log(orderstring5);
+        }
+        if  (currentOrder == 6) {
+            orderstring6.push(item);
+            ordercolor6.push(type);
+            console.log(orderstring6);
+        }
+       
+        
+        
     }
-    if  (currentOrder == 2) {
-        orderstring2.push(item);
-        ordercolor2.push(type);
-        console.log(orderstring2);
-    }
-    if  (currentOrder == 3) {
-        orderstring3.push(item);
-        ordercolor3.push(type);
-        console.log(orderstring3);
-    }
-    if  (currentOrder == 4) {
-        orderstring4.push(item);
-        ordercolor4.push(type);
-        console.log(orderstring4);
-    }
-    if  (currentOrder == 5) {
-        orderstring5.push(item);
-        ordercolor5.push(type);
-        console.log(orderstring5);
-    }
-    if  (currentOrder == 6) {
-        orderstring6.push(item);
-        ordercolor6.push(type);
-        console.log(orderstring6);
-    }
-    if (side) {
-        page = 7;
-        redraw();
-    }
+     if (side) {
+            page = 7;
+            redraw();
+        }
 }
 //Makes button for menu
 function createMenuItem(name, value, type, side) {
@@ -74,14 +110,44 @@ function createMenuItem(name, value, type, side) {
     }
     return li;
 }
+function createCustomMod() {
+    let li = document.createElement('button');
+    li.innerHTML = "Custom";
+    li.style = "background-color: #2c2f33; border: 1px solid gray; color: white; padding: 32px 140px; text-align: center; text-decoration: none; font-size: 16px; width: 97%; display:  block; ";
+    li.onclick = function () {
+        let result = window.prompt("Text", "");
+        addToOrder(result, 3);
+        
+    }
+    return li;
+}
+function createNo() {
+    let li = document.createElement('button');
+    li.innerHTML = "No"
+    li.style = "background-color: #2c2f33; border: 1px solid gray; color: white; padding: 32px 140px; text-align: center; text-decoration: none; font-size: 16px; width: 97%; display:  block; ";
+    li.onclick = function () {
+        if (addNo == true) {
+            addNo = false;
+             li.style = "background-color: #2c2f33; border: 1px solid gray; color: white; padding: 32px 140px; text-align: center; text-decoration: none; font-size: 16px; width: 97%; display:  block; ";
+        } else {
+            addNo = true;
+             li.style = "background-color: red; border: 1px solid gray; color: white; padding: 32px 140px; text-align: center; text-decoration: none; font-size: 16px; width: 97%; display:  block; ";
+        } 
+        
+    }
+    return li;
+}
 function createModSideItem(name, value, type) {
     let li = document.createElement('button');
     li.innerHTML = name;
     li.style = "background-color: #2c2f33; border: 1px solid gray; color: white; padding: 32px 140px; text-align: center; text-decoration: none; font-size: 16px; width: 97%; display:  block; ";
     li.onclick = function () {
       addToOrder(value, type, false);
-      page = 8;
-      redraw();
+      if (type == 2) {
+          page = 8;
+          redraw();
+      }
+      
     }
     return li;
 }
@@ -276,15 +342,19 @@ if (page == 1) {
 
         
     if (page == 2) {
-    menu.appendChild(createMenuItem('Lemonade',"Lemonade",1,false));
+        menu.appendChild(createMenuItem('Lemonade',"Lemonade",1,false));
         menu.appendChild(createMenuItem('Coke',"Coke",1,false));
         menu.appendChild(createMenuItem('Diet Coke',"Diet Coke",1,false));
+        menu.appendChild(createMenuBack());
         
    
 }   
      if (page == 4) {
         menu.appendChild(createMenuItem('WZ Classic',"WZ Classic",1,true));
+        menu.appendChild(createMenuItem('Royale', "Royale", 1, true));
+        
         menu.appendChild(createMenuBack());
+        
         
         
    
@@ -312,6 +382,20 @@ if (page == 1) {
         menu.appendChild(createModSideItem('House sld',"House Salad",2));
         
         
+    }
+    if (page == 8) {
+        menu.appendChild(createMenuBack());
+        menu.appendChild(createCustomMod());
+        menu.appendChild(createNo());
+        menu.appendChild(createModSideItem('No bun',"No bun",3));
+        menu.appendChild(createModSideItem('Rare',"Rare",3));
+        menu.appendChild(createModSideItem('Med Rare',"Meduim  Rare",3));
+        menu.appendChild(createModSideItem('Med',"Meduim",3));
+        menu.appendChild(createModSideItem('Med Well',"Med Well",3));
+        menu.appendChild(createModSideItem('Well Done', "Well Done", 3));
+        menu.appendChild(createModSideItem('No  LTO', "No LTO", 3));
+        
+       
     }
 
 
